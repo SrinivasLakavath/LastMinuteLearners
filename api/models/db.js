32b04fca86f9cb859d7d9297dcb214d0bd972f74
SrinivasLakavath <lakavathsrinivas89@gmail.com>
2024-09-25 08:18:50 +0530

    5th commit

--- api/models/db.js
+++ api/models/db.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
+require('dotenv').config();  // Load environment variables from .env file
 
-const dbURI = "mongodb+srv://22eg106b31:123@cluster0.sn08o.mongodb.net/LML?retryWrites=true&w=majority"; // Update with your MongoDB URI
+// const dbURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/LML'; 
+
+const dbURI = process.env.MONGODB_URI || 'mongodb+srv://22eg106b31:123@cluster0.sn08o.mongodb.net/LML?retryWrites=true&w=majority'; // Update with your MongoDB URI
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
--- api/models/dist/db.dev.js
+++ api/models/dist/db.dev.js
@@ -2,7 +2,11 @@
 
 var mongoose = require('mongoose');
 
-var dbURI = "mongodb+srv://22eg106b31:123@cluster0.sn08o.mongodb.net/LML?retryWrites=true&w=majority"; // Update with your MongoDB URI
+require('dotenv').config(); // Load environment variables from .env file
+// const dbURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/LML'; 
+
+
+var dbURI = process.env.MONGODB_URI || 'mongodb+srv://22eg106b31:123@cluster0.sn08o.mongodb.net/LML?retryWrites=true&w=majority'; // Update with your MongoDB URI
 
 mongoose.connect(dbURI, {
   useNewUrlParser: true,
